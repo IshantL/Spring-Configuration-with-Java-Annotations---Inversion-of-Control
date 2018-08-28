a4d2fab94c2400499485b5f79a9ac594b870e9c1
IshantL <ishantlambhate@gmail.com>
2018-08-28 08:52:19 +0000

    Spring Configuration with Java Code

---
+++ src/com/ishant/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,24 @@
+package com.ishant.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class JavaConfigDemoApp {
+
+	public static void main(String[] args) {
+	//read spring config file
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+	//get the bean from cotaoiner
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+	//call a method on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
+		
+	//close the context
+		context.close();
+	}
+
+}
+
+/*WARNING: Exception encountered during context initialization - cancelling refresh attempt: org.springframework.beans.factory.UnsatisfiedDependencyException: Error creating bean with name 'tennisCoach': Unsatisfied dependency expressed through field 'fortuneService'; nested exception is org.springframework.beans.factory.NoUniqueBeanDefinitionException: No qualifying bean of type 'com.ishant.springdemo.FortuneService' available: expected single matching bean but found 4: databaseFortuneService,happyFortuneService,randomService,restFortuneService
+Exception in thread "main" org.springframework.beans.factory.UnsatisfiedDependencyException: Error creating bean with name 'tennisCoach': Unsatisfied dependency expressed through field 'fortuneService'; nested exception is org.springframework.beans.factory.NoUniqueBeanDefinitionException: No qualifying bean of type 'com.ishant.springdemo.FortuneService' available: expected single matching bean but found 4: databaseFortuneService,happyFortuneService,randomService,restFortuneService*/
---
+++ src/com/ishant/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.ishant.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ishant.springdemo")
+public class SportConfig {
+
+}
